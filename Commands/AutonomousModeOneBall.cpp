635d366f81d1c2f03b66f9b61e99bcae066c28d9
Zesty-Zak <ZakariaASekhri@gmail.com>
2014-12-15 15:54:10 -0500

    zizzag assisnment
    I did not finish it

--- Commands/AutonomousModeOneBall.cpp
+++ Commands/AutonomousModeOneBall.cpp
@@ -14,14 +14,10 @@
 
 AutonomousModeOneBall::AutonomousModeOneBall()
 {
-  AddSequential(new AutonomousLowGearCommand());
-  AddSequential(new LoadCatapultGroup());
-  AddSequential(new OperatorLowCommand());
-  AddSequential(new AutonomousPickupCommand(2.0));
-  AddSequential(new OperatorPickup2Command());
-  AddSequential(new OperatorHighCommand());
-  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
-  AddParallel(new OperatorPickup2Command(0.5));
-  AddSequential(new AutonomousCoastCommand(1.0f, TURN_CORRECTION, 0.0f, TURN_CORRECTION, 0.5));
-  AddSequential(new DriveLaunchReleaseCommand());
+  
+  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 858585858588585));
+  AddSequential(new AutonomousDriveCommand(0.9f, TURN_CORRECTION, 0));
+  wait 2;
+  AddSequential(new AutonomousDriveCommand
+  
 }
